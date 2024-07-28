5bca0f1231d1135e0148f0996988f6ca776110ce
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2024-07-28 05:33:57 -0300

    feat: add error handler

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -3,14 +3,14 @@ import multer from 'multer'
 import * as uploadController from "../controllers/upload"
 
 const upload = multer({
-  dest: './tmp'
+  dest: './tmp',
+  fileFilter: (req, file, callback) => {
+    const allowed: string[] = ['image/jpg', 'image/jpeg', 'image/png']
+    callback(null, allowed.includes(file.mimetype))
+  },
+  limits: { fieldNameSize: 20000000 }
 })
 
 export const router = Router()
 
-router.post('/upload', upload.fields(
-  [
-    { name: 'avatar', maxCount: 1 },
-    { name: 'gallery', maxCount: 3 }
-  ]
-), uploadController.uploadFile)
+router.post('/upload', upload.single('avatar'), uploadController.uploadFile)
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import express, { urlencoded } from 'express'
+import express, { ErrorRequestHandler, urlencoded } from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 import helmet from 'helmet'
 import { router } from './routes/main'
+import { MulterError } from 'multer'
 
 const server = express()
 server.use(helmet())
@@ -12,6 +13,20 @@ server.disable('x-pwered-by')
 server.use(express.json())
 
 server.use(router)
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  res.status(400)
+
+  if (err instanceof MulterError) {
+    res.json({ error: err.code })
+  }
+  else {
+    console.log(err)
+    res.json({ error: 'An error has occured' })
+  }
+}
+
+server.use(errorHandler)
 
 const port = process.env.PORT || 8000
 server.listen(port, () => {
