01ec363517dcc78c82710525691d8475a443fb1c
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2024-07-29 04:17:04 -0300

    feat: remove tmp files and add image to media

--- src/controllers/upload.ts
+++ src/controllers/upload.ts
@@ -1,6 +1,24 @@
 import { RequestHandler } from "express"
+import { unlink } from "fs/promises"
+import sharp from "sharp"
+
+sharp.cache(false)
 
 export const uploadFile: RequestHandler = async (req, res) => {
-  console.log(req.files)
-  res.json({})
+  if (req.file) {
+    const filename = `${req.file.filename}.jpg`
+
+    await sharp(req.file.path)
+      .resize(300, 300)
+      .toFormat('jpeg')
+      .toFile(`./public/media/${filename}`)
+
+    await unlink(req.file.path)
+
+    res.json({ image: `${filename}` })
+  }
+  else {
+    res.status(400)
+    res.json({ error: 'Invalid file' })
+  }
 }
